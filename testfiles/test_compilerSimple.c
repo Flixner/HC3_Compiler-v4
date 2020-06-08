75d4ca77e45b7ab78f631a1ca56fa709271eccd4
Felix Rauterberg <felix-rauterberg@gmx.de>
2020-06-08 12:10:05 +0200

    Merge

--- testfiles/test_compilerSimple.c
+++ testfiles/test_compilerSimple.c
@@ -1,10 +1,16 @@
+short asdf(short a)
 {
+    return 2;
+}
 
 short main()
 {
     char z = 'h';
     char y = 30;
     char v = '3';
+    short tt = 32;
     short ttt;
+    ttt = asdf(tt);
+
     return 0;
 }
--- testfiles/test_compilerSimple_preprocessed.c
+++ testfiles/test_compilerSimple_preprocessed.c
@@ -1,9 +1,14 @@
+#line 1 "testfiles/test_compilerSimple.c"
 void neu(short i)
+{
+    i++;
+}
 short main()
 {
     char z = 'h';
     char y = 30;
     char v = '3';
+    short a = 1;
     neu(a);
     return 0;
 }
