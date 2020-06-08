39b4b4382e2ee45c4574519cc720a0bf30ffead1
Fail0X0 <felix.haldorn@yahoo.de>
2020-06-08 11:33:41 +0200

    comment char

--- testfiles/test_compilerSimple.asm
+++ testfiles/test_compilerSimple.asm
@@ -4,8 +4,8 @@ main:
 	LLI r22 0xFFDF ;		
 	MOV r1 r22 ;		
 	SUBI r1 3 ;		    reserve space for local variables on stack
-	LUI r22 104 ;		    load Constant
-	LLI r22 104 ;		    load Constant
+	LUI r22 104 ;		    load Constant char
+	LLI r22 104 ;		    load Constant char
 	MOV r10 r22 ;		move to expected position
 	MOV r15 r1 ;		calculate Adress of
 	ADDI r15 1 ;		z in Compound 1
@@ -16,8 +16,8 @@ main:
 	MOV r15 r1 ;		calculate Adress of
 	ADDI r15 2 ;		y in Compound 1
 	STORE r10 r15 ;		and store result
-	LUI r22 51 ;		    load Constant
-	LLI r22 51 ;		    load Constant
+	LUI r22 51 ;		    load Constant char
+	LLI r22 51 ;		    load Constant char
 	MOV r10 r22 ;		move to expected position
 	MOV r15 r1 ;		calculate Adress of
 	ADDI r15 3 ;		v in Compound 1
--- testfiles/test_compilerSimple.c
+++ testfiles/test_compilerSimple.c
@@ -4,6 +4,6 @@ short main()
     char z = 'h';
     char y = 30;
     char v = '3';
-    
+
     return 0;
 }
